8ad01ad0185c4643891588b51f62edce04600a28
Amrita Bindukalpa <amrita.bindukalpa@skybettingandgaming.com>
2024-07-23 16:34:15 +0100

    initial commit

---
+++ skyVegasTests/app.spec.ts
@@ -0,0 +1,82 @@
+import { test, expect, type Page } from "@playwright/test";
+import { assert, log } from "console";
+
+test("Page OK", async ({ page }) => {
+  await page.goto("https://skyvegas.com.nxt.ppbdev.com/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(
+    "Online Vegas Games | 50 Free Spins | SBG Vegas"
+  );
+});
+
+test("Test login works", async ({ page }) => {
+  await page.goto("https://skyvegas.com.nxt.ppbdev.com/");
+  await page.getByRole("link", { name: "Log In" }).click();
+  await page.getByLabel("Username").fill("rgsSBGbNTPp");
+  await page.getByLabel("PIN").fill("");
+  await page.getByRole("button", { name: "I Accept" }).click();
+  page.getByRole("button", { name: "Log in" }).focus;
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  await expect(page).toHaveURL("https://skyvegas.com.nxt.ppbdev.com/");
+  await expect(page).toHaveTitle(
+    "Online Vegas Games | 50 Free Spins | SBG Vegas"
+  );
+  await expect(
+    page.getByText("CONGRATULATIONS TO OUR LUCKY WINNER")
+  ).toBeVisible();
+  await page.goto("https://skyvegas.com.nxt.ppbdev.com/c/pragmatic-games");
+});
+
+test("PragGames", async ({ page }) => {
+  await page.goto("https://skyvegas.com.nxt.ppbdev.com/");
+  await page.getByRole("link", { name: "Log In" }).click();
+  await page.getByLabel("Username").fill("rgsSBGbNTPp");
+  await page.getByLabel("PIN").fill("");
+  await page.getByRole("button", { name: "I Accept" }).click();
+  page.getByRole("button", { name: "Log in" }).focus;
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  await expect(page).toHaveURL("https://skyvegas.com.nxt.ppbdev.com/");
+  await expect(page).toHaveTitle(
+    "Online Vegas Games | 50 Free Spins | SBG Vegas"
+  );
+  await expect(
+    page.getByText("CONGRATULATIONS TO OUR LUCKY WINNER")
+  ).toBeVisible();
+  await page.goto("https://skyvegas.com.nxt.ppbdev.com/c/pragmatic-games/");
+  const linksCount = await page
+    .getByRole("link", { name: "Real Play" })
+    .count();
+  expect(linksCount).toBe(11);
+
+  for (let i = 0; i < linksCount; i++) {
+    var link = await page.getByRole("link", { name: "Real Play" }).nth(i);
+    const consoleMessages: string[] = [];
+    await link.click();
+    page.on("console", (msg) => {
+      consoleMessages.push(msg.text());
+    });
+
+    const expectedMessage = '"loadingPercent":100';
+    const expectedMessageLiveGames = "gameLoadComplete";
+    try {
+      await expect(async () => {
+        const messageExists = consoleMessages.some(
+          (msg) =>
+            msg.includes(expectedMessage) ||
+            msg.includes(expectedMessageLiveGames)
+        );
+        expect(messageExists).toBe(true);
+      }).toPass({
+        timeout: 60_000,
+      });
+    } catch (error) {
+      await page.screenshot({ path: "failure_gamenumber[" + i + "].png" });
+    }
+
+    //expect(page).toHaveScreenshot();
+    await page.goto("https://skyvegas.com.nxt.ppbdev.com/c/pragmatic-games/");
+  }
+});
