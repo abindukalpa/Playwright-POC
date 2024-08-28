a65364f42124086278b4d6f308c9c7c72afe35d5
Amrita Bindukalpa <amrita.bindukalpa@skybettingandgaming.com>
2024-08-28 11:11:23 +0100

    Check css being attached to the sound icon

--- skyVegasTests/soundCheckGameClient.spec.ts
+++ skyVegasTests/soundCheckGameClient.spec.ts
@@ -1,4 +1,4 @@
-import { test, type Page } from "@playwright/test";
+import { test, type Page, expect } from "@playwright/test";
 import {
   launchGame,
   validateConsoleMessages,
@@ -12,6 +12,7 @@ import { ExpectedMessage } from "../types/expectedMessage";
 readGames().forEach((game) => {
   const consoleMessages: string[] = [];
   let page: Page;
+  let classAttribute: string;
   test.describe(`Testing with text: ${game}`, () => {
     test.beforeAll(async ({ browser }) => {
       page = await browser.newPage();
@@ -23,19 +24,30 @@ readGames().forEach((game) => {
     });
 
     test("Test sound toggle", async () => {
+      startEventListener(page, consoleMessages);
+      await launchGame(page, game, consoleMessages);
       const soundToggleIcon = page
         .frameLocator("#root iframe")
         .locator("i")
         .nth(2);
-      startEventListener(page, consoleMessages);
-      await launchGame(page, game, consoleMessages);
+
+      classAttribute = (await soundToggleIcon.getAttribute("class")) as string;
+      if (classAttribute == "icon icon-volume") {
+        //If sound is on, turn it off
+        await soundToggleIcon.click();
+      }
       await soundToggleIcon.click();
-      await soundToggleIcon.click();
+      expect(await soundToggleIcon.getAttribute("class")).toEqual(
+        "icon icon-volume"
+      );
       await validateConsoleMessages(
         ExpectedMessage.SOUND_CHECK_GAME_ON,
         consoleMessages
       );
-
+      await soundToggleIcon.click();
+      expect(await soundToggleIcon.getAttribute("class")).toEqual(
+        "icon icon-volume muted"
+      );
       await validateConsoleMessages(
         ExpectedMessage.SOUND_CHECK_GAME_OFF,
         consoleMessages
