13029c75719256705719507e6c26c2eb39cf2866
emma wilkinson <emma.wilkinson2@flutteruki.com>
2024-09-04 14:57:36 +0100

    code updated from code review comments

--- skyVegasTests/utilities/launchGameHelper.ts
+++ skyVegasTests/utilities/launchGameHelper.ts
@@ -1,4 +1,5 @@
 import { expect, type Page } from '@playwright/test';
+
 import { validateConsoleMessages } from './validateConsoleMessagesHelper';
 import { ExpectedMessage } from '../../types/expectedMessage';
 
@@ -8,7 +9,7 @@ export const launchGame = async (
     consoleMessages: string[]
 ) => {
     const searchForGamesText = 'Search for games...'
-    await expect(page.locator('table.ssc-wldw tbody')).toContainText('£');
+    await expect(page.locator('table.ssc-wldw tbody')).not.toContainText("Loading...");
     await page.getByText(searchForGamesText).click();
     await page.getByPlaceholder(searchForGamesText).fill(gameName);
     await expect(page.locator('.search-box .game-tile-from-search-component .tile-container').first()).toBeAttached()
