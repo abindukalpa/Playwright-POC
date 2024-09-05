7759029dc51161d1803c3f607f7f19cb97f88af7
Amrita Bindukalpa <amrita.bindukalpa@skybettingandgaming.com>
2024-09-05 13:33:09 +0100

    Changes to fix game search bug

--- skyVegasTests/utilities/launchGameHelper.ts
+++ skyVegasTests/utilities/launchGameHelper.ts
@@ -8,15 +8,36 @@ export const launchGame = async (
     gameName,
     consoleMessages: string[]
 ) => {
-    const searchForGamesText = 'Search for games...'
-    await expect(page.locator('table.ssc-wldw tbody')).not.toContainText("Loading...");
+    const searchForGamesText = 'SystemSearchSearch for games';
+    const placeholderText = 'Search for games...';
+
+    await expect(async () => {
+        let amount = await page.locator('table.ssc-wldw tbody').textContent();
+        const numberRegex = /[\d,]+\.\d{2}/g;
+        const numbers = amount?.match(numberRegex);
+        expect(numbers?.length).toEqual(2); //expect 2 numbers Main and Bonus
+    }).toPass({
+        intervals: [5_000, 10_000, 15_000],
+        timeout: 60_000,
+    });
+
     await page.getByText(searchForGamesText).click();
-    await page.getByPlaceholder(searchForGamesText).fill(gameName);
-    await expect(page.locator('.search-box .game-tile-from-search-component .tile-container').first()).toBeAttached()
-    await page.locator('.search-box .game-tile-from-search-component .tile-container').first().click();
+
+    await page.getByPlaceholder(placeholderText).fill(gameName);
+    await expect(
+        page
+            .locator(
+                '.search-box .game-tile-from-search-component .tile-container'
+            )
+            .first()
+    ).toBeAttached();
+    await page
+        .locator('.search-box .game-tile-from-search-component .tile-container')
+        .first()
+        .click();
 
     await validateConsoleMessages(
         ExpectedMessage.GAME_LOAD_COMPLETE,
-        consoleMessages,
+        consoleMessages
     );
 };
