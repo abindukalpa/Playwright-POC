323b5285f13c8545a7317cd9891e071be32bc405
Catalin Mares <catalin.mares@flutteruki.com>
2024-09-20 14:01:39 +0100

    GTX-512 - remove console logs used for debugging

--- skyVegasTests/utilities/recoverFromFailureHelper.ts
+++ skyVegasTests/utilities/recoverFromFailureHelper.ts
@@ -5,20 +5,19 @@ import { ExpectedMessage } from '../../types/expectedMessage';
 export const recoverFromFreeSpins = async (
     page: Page,
     consoleMessages: string[]
-) => {
+): Promise<void> => {
     const playModeUpdateConsoleMessages: string[] = consoleMessages.filter(
         (_) => _.includes(ExpectedMessage.PLAY_MODE_UPDATE)
     );
     const isFreeSpinModeOn: boolean = playModeUpdateConsoleMessages.some((_) =>
         _.includes(ExpectedMessage.FREE_SPIN_PLAY_MODE_UPDATE)
     );
-    console.log(isFreeSpinModeOn);
+
     if (isFreeSpinModeOn) {
         // click on the canvas to start the free spins, they will start auto-playing
         // sometimes there are multiple rounds so the test will need to click again to start the next round, this will be done upon test retry
         await expect(async (): Promise<void> => {
             await page.mouse.click(300, 300);
-            console.log('clicking again');
             expect(
                 messageExists(
                     consoleMessages,
